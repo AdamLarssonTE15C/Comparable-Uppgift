84ad659e90deddf6bf9259d9c02e4cf6918579dd
AdamLarssonTE15C <DIN MAIL>
2017-11-21 10:31:11 +0100

    Space Index
    Har skapat en sträng för att skilja mellan Förnamn och Efternamn genom att sätta in när det ska delas i vilket speciellt index.

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,45 @@
 
-public class Person {
+	public class Person implements Comparable<Person>{
+		
+		private String FirstName;
+		private String LastName;
+		
+		public Person(String fullname){
+			
+			int space = fullname.indexOf(' ');
+			
+			this.LastName = fullname.substring(0, space);
+			this.FirstName = fullname.substring(space+1);
+			
+		}
+		
 
-}
+		
+		public String getFirstName() {
+			return FirstName;
+		}
+
+
+		public void setFirstName(String firstName) {
+			FirstName = firstName;
+		}
+
+
+		public String getLastName() {
+			return LastName;
+		}
+
+
+		public void setLastName(String lastName) {
+			LastName = lastName;
+		}
+
+
+		public int compareTo(Person arg0) {
+			
+			return 0;
+		}
+		
+
+	}
+
