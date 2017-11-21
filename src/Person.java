0908e3f2fec3a76e6f625518354941bea6259a1a
AdamLarssonTE15C <DIN MAIL>
2017-11-21 10:17:59 +0100

    Variabler
    Har skapat två variabler med namn FirstName och LastName

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,34 @@
 
-public class Person {
+public class Person implements Comparable<Person>{
+	
+	private String FirstName;
+	private String LastName;
+
+	
+	public String getFirstName() {
+		return FirstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return LastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+
+	public int compareTo(Person arg0) {
+		
+		return 0;
+	}
+	
 
 }
