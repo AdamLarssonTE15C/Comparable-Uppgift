e9dfbdb4f5ddffe2980c178e49bf568296701273
AdamLarssonTE15C <DIN MAIL>
2017-11-28 10:06:40 +0100

    Person
    Jag har skapat en variabel vid namn Person (p) som kollar på nästa linje hela tiden, 10 gånger.  Den omvandlar också orden på det rätta sättet.  Sedan gör jag också en for loop som kollar Person vilket sedan countrar Person igen. Sedan skriver programmet ut omvandlingen av LastName och adderar FirstName.

--- src/RunPerson.java
+++ src/RunPerson.java
@@ -1,4 +1,32 @@
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.util.ArrayList;
+	import java.util.Collections;
+	import java.util.Scanner;
 
-public class RunPerson {
+
+	public class RunPerson {
+
+		
+		public static void main(String[] args) throws FileNotFoundException {
+			
+			Scanner sc = new Scanner(new File("Names.txt"));
+			
+			ArrayList<Person> Person = new ArrayList<Person>();
+			
+			for(int i = 0; i < 10 ; i++){
+			
+			Person p = new Person (sc.nextLine().trim());
+			Person.add(p);
+			
+			
+			}
+			
+			Collections.sort(Person);
+			
+			for(Person counter: Person);
+				System.out.println(counter.getLastName +" "+counter.getFirstName());
+			
+		}
 
 }
